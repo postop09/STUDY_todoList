7c14d56120406046f908b26ff49c2136bae9e76b
조윤식 <postop@tuneit.io>
2023-01-06 13:49:24 +0900

    #3 [ToDoList] - 로그아웃 구현

--- src/component/layout/Header.tsx
+++ src/component/layout/Header.tsx
@@ -1,11 +1,32 @@
 import React from "react";
+import Button from "../Button";
+import styled from "styled-components";
+import { useNavigate } from "react-router-dom";
 
 const Header = () => {
+  const navigate = useNavigate();
+
+  const onRemoveAuthToken = () => {
+    localStorage.removeItem("Authorization");
+    alert("로그아웃 되었습니다.");
+    navigate("/auth");
+  };
+
   return (
-    <header>
-      <h1>ToDoList</h1>
-    </header>
+    <HeaderWrapper>
+      <h1>원티드 프리온보딩 챌린지</h1>
+      <div>
+        <Button onClick={onRemoveAuthToken}>로그아웃</Button>
+      </div>
+    </HeaderWrapper>
   );
 };
 
 export default Header;
+
+const HeaderWrapper = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1rem 2rem;
+`;
